cda3734e3e39a03b1e3666b1825838078f09c459
Markus Schmidl <markus.schmidl@ymail.com>
2017-04-05 14:43:44 +0200

    added schilD_elabor config

---
+++ configs/schild_elabor.h
@@ -0,0 +1,11 @@
+#ifndef CONFIG_H__
+#define CONFIG_H__
+
+#define MAC         {0x70,0x69,0x69,0x2d,0x30,0x32}
+
+#define IP          {172,23,92,18}
+#define BROADCAST   {172,23,92,255}
+
+#define WS2812_LEDS 64
+
+#endif
